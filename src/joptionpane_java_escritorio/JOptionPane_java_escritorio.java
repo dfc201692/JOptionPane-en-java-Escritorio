f82ff835b1a26cf80e30c11e222da8e4744811ba
david.cruz <dfcc15@hotmail.com>
2021-06-15 11:22:07 -0500

    JOptionPane

---
+++ src/joptionpane_java_escritorio/JOptionPane_java_escritorio.java
@@ -0,0 +1,15 @@
+package joptionpane_java_escritorio;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author david.cruz
+ */
+public class JOptionPane_java_escritorio {
+
+    public static void main(String[] args) {
+
+        Principal.main(args);
+
+    }
+}
